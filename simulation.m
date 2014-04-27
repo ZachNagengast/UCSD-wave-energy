734ab713dc2c3af0dccbeadfaa884149fe5e5dae
kfructuoso <atreyu80392@aol.com>
2014-04-27 12:47:17 -0700

    minor changes and tried different methods

--- simulation.m
+++ simulation.m
@@ -3,10 +3,12 @@
 %Kevin - eventually we'll want to make it a function but to make sure it
 %works now, I'll start by leaving it a script.  Makin it a function later
 %should be easy
+%unsure of how inertia plays into equations based on Anderson's derivation
+%seems like it's only ratio of inerta?
 
 %conversion factors
 in_m = .0254;           %multiply inches to get meters, divide meters to get inches
-rpm_radps = 2*pi*60;    %multiply rpm to get rad/s, divide rad/s to get rpm
+rpm_radps = 2*pi/60;    %multiply rpm to get rad/s, divide rad/s to get rpm
 g = 9.81;               %m/s^2
 
 %Initialize known variables
@@ -14,13 +16,13 @@ r = 6;                  %in
 thick = .25;            %in
 density = 1400;         %kg/m^3 acrylic from http://www.avlandesign.com/density_construction.htm
 omega_rotor = 3000;     %rpm
-wave_freq = .125;          %Hz *** I used same parameters as in paper from Oscar
+wave_freq = .125;       %Hz *** I used same parameters as in paper from Oscar
 wave_mag = 1.5;         %m
 wave_period = 1/wave_freq; %seconds
 
 %Convert radius and thickness
-r = r*in_m;
-thick = thick*in_m;
+r = r*in_m;             %m
+thick = thick*in_m;     %m
 
 %Resulting parameters
 area = pi*r^2;          %m^2
@@ -39,4 +41,16 @@ time = 0:0.01:10;       %seconds
 alpha_outer = ((2*pi/wave_period)^4)*(wave_mag/g)* ...  %derivation eq 89 in paper
     sin(2*pi*time/wave_period);   %array of accelerations outer gimbal over time
 
-omega_inner = alpha_outer/(2*omega_rotor); %not sure if this is right but this is based from andersons equation slide eq 5
+%try 1 - not sure if this is right but this is based from andersons equation slide eq 5
+omega_inner1 = alpha_outer/(2*omega_rotor*rpm_radps); 
+
+%try 2 - omega_inner = omega_gyro + omega_initial
+for n=1:length(alpha_outer)-1
+    omega_inner(n+1) = alpha_outer(n)/(2*omega_rotor*rpm_radps)+omega_inner(n);
+end
+
+%try 3 - dtheta method
+dtheta_inner = alpha_outer/(2*omega_rotor*rpm_radps) * 0.01;
+for n=1:length(dtheta_inner)-1
+    theta_inner(n+1) = theta_inner(n)+dtheta_inner(n);
+end
